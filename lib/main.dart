fdf1a4762034a65dedecbb3646beac3efa5bcdbb
Christophe Duchesne <cduchesne.1@gmail.com>
2024-01-25 21:34:51 -0500

    chore: setup injection container

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import 'injection_container.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  setupLocator();
 
   runApp(DevicePreview(
     builder: (context) => const MyApp(),
