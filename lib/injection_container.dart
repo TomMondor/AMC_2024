e57f0913f45f784f5474d8e51e3b3804d70eccba
Thomas Mondor <thomas.mondor.1@ulaval.ca>
2024-01-27 12:45:00 -0500

    added http dependancy

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:get_it/get_it.dart';
 
 import 'src/application/auth_service.dart';
@@ -7,4 +9,7 @@ final locator = GetIt.instance;
 void setupLocator() {
   // Services
   locator.registerLazySingleton(() => AuthService());
+
+  //API
+  // locator.registerLazySingleton<HttpClient>(() => HttpClientImpl(locator()));
 }
---
+++ lib/src/infra/electricity/hydro_api.dart
@@ -0,0 +1,4 @@
+
+// var getHydroData() async {
+
+// }
