000f3e27458fcde2af5596fb2b8c8a7c62f35849
David Ferland <david.ferland.swe@gmail.com>
2024-01-26 22:26:19 -0500

    login

--- lib/src/view/account/login.dart
+++ lib/src/view/account/login.dart
@@ -5,6 +5,58 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "WelCum to AMC",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 200,
+                height: 50,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 200,
+              height: 50,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () => {print("Login")},
+                child: const Text('Login'),
+              ),
+            ),
+            const TextButton(
+                onPressed: null,
+                child: Text(
+                    'Forgot Password?',
+                    style: TextStyle(color: Colors.blueAccent),
+                ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
