258616808daf3dcbcdb76c6f30743f2d1424fe18
Jonathan Bessette <JOBES10@ulaval.ca>
2024-01-27 13:27:47 -0500

    color theme and fonts

---
+++ lib/src/theme/colors.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+const Color kcPrimary = Color(0xff0b6623);
+const Color kcPrimaryVariant = Color(0xff3b4664);
+const Color kcOnPrimary = Colors.white;
+const Color kcSecondary = Color(0xff3bb143);
+const Color kcSecondaryVariant = Color(0xff242424);
+const Color kcOnSecondary = Colors.white;
+const Color kcLightSecondary = Color(0xffaeb3c0);
+const Color kcBackground = Colors.white;
+const Color kcOnBackground = kcSecondary;
+const Color kcSurface = Colors.white;
+const Color kcOnSurface = kcSecondary;
+const Color kcError = Color(0xfffd687d);
+const Color kcOnError = Colors.white;
+
+const Color kcGold = Color(0xffEEAC14);
+const Color kcPlatinum = Color(0xff7E7E7E);
+Color kcShadowColor = Colors.black.withOpacity(0.15);
+const Color kcPink = Color(0xffE6506C);
+
+const ColorScheme colorScheme = ColorScheme(
+  primary: kcPrimary,
+  secondary: kcSecondary,
+  surface: kcSurface,
+  background: kcBackground,
+  error: kcPrimary,
+  onPrimary: kcOnPrimary,
+  onSecondary: kcOnSecondary,
+  onSurface: kcOnSurface,
+  onBackground: kcOnBackground,
+  onError: kcOnError,
+  brightness: Brightness.light,
+);
---
+++ lib/src/theme/fonts.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+const TextTheme fonts = TextTheme(
+  displayLarge: TextStyle(
+    fontSize: 20,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.bold,
+  ),
+  headlineLarge: TextStyle(
+    fontSize: 90,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.bold,
+  ),
+  headlineMedium: TextStyle(
+    fontSize: 38,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w800,
+  ),
+  bodyMedium: TextStyle(
+    fontSize: 16,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+  ),
+  titleMedium: TextStyle(
+    fontSize: 20,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w800,
+  ),
+  bodySmall: TextStyle(
+    fontSize: 14,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+  ),
+  labelLarge: TextStyle(
+    fontSize: 16,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.bold,
+  ),
+);
