314777113bd1708d6e2d4168e406f07bd61b0682
Babouche <clnabergel@hotmail.fr>
2024-01-28 07:25:38 -0500

    Plug API

--- lib/src/view/home/home.dart
+++ lib/src/view/home/home.dart
@@ -1,11 +1,14 @@
 import 'package:amc_2024/src/application/auth_service.dart';
 import 'package:amc_2024/src/infra/account/user_repo.dart';
+import 'package:amc_2024/src/infra/http_client.dart';
 import 'package:amc_2024/src/theme/colors.dart';
 import 'package:amc_2024/src/view/widgets/card_carbon.dart';
 import 'package:amc_2024/src/view/widgets/card_summary.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:geolocator/geolocator.dart';
 
 import '../../../injection_container.dart';
 import '../widgets/card_tip.dart';
@@ -13,9 +16,52 @@ import '../widgets/card_tip.dart';
 class Home extends HookWidget {
   const Home({super.key});
 
+  String getAqiText(int aqi) {
+    if (aqi < 100) {
+      return "Good";
+    } else if (aqi < 200) {
+      return "Moderate";
+    }
+    return "Unhealthy";
+  }
+
+  String getAqiRange(int aqi) {
+    if (aqi < 100) {
+      return "1-100";
+    } else if (aqi < 200) {
+      return "101-200";
+    }
+    return "200+";
+  }
+
   @override
   Widget build(BuildContext context) {
     final name = useState("");
+    final airPollution = useState(0);
+    final temperature = useState(0);
+    final weatherCondition = useState("");
+
+    Future<void> fetchWeatherData(double lat, double lon) async {
+      const apiKey = "e452e82674cf72c80b50ffe67996f2b6";
+      final apiUrl =
+          "https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey";
+
+      HttpClientImpl client = HttpClientImpl(locator());
+      await client.get(Uri.parse(apiUrl).toString(), "").then((value) => {
+            temperature.value = (value.data["main"]["temp"] - 273.15).toInt(),
+            weatherCondition.value = value.data["weather"][0]["main"]
+          });
+    }
+
+    Future<void> fetchAirPollutionData(double lat, double lon) async {
+      const apiKey = "e452e82674cf72c80b50ffe67996f2b6";
+      final apiUrl =
+          "http://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=$lat&lon=$lon&appid=$apiKey";
+
+      HttpClientImpl client = HttpClientImpl(locator());
+      await client.get(Uri.parse(apiUrl).toString(), "").then(
+          (value) => airPollution.value = value.data["list"][0]["main"]["aqi"]);
+    }
 
     useEffect(() {
       Future<void> getUserName() async {
@@ -33,7 +79,8 @@ class Home extends HookWidget {
 
     useEffect(() {
       Future<void> requestNotificationPermissions() async {
-        NotificationSettings settings = await FirebaseMessaging.instance.requestPermission(
+        NotificationSettings settings =
+            await FirebaseMessaging.instance.requestPermission(
           alert: true,
           announcement: false,
           badge: true,
@@ -45,6 +92,16 @@ class Home extends HookWidget {
 
         print('User granted permission: ${settings.authorizationStatus}');
       }
+
+      Future<void> getCurrentLocation() async {
+        Position position = await Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.high);
+
+        await fetchWeatherData(position.latitude, position.longitude);
+        await fetchAirPollutionData(position.latitude, position.longitude);
+      }
+
+      getCurrentLocation();
 
       requestNotificationPermissions();
       return () {};
@@ -73,22 +130,26 @@ class Home extends HookWidget {
                 children: const [
                   CardTip(
                     topIcon: Icons.whatshot,
-                    descriptionText: "A slow period is starting, do you have a laundry load to do?",
+                    descriptionText:
+                        "A slow period is starting, do you have a laundry load to do?",
                     titleText: "Slack period",
                   ),
                   CardTip(
                     topIcon: Icons.house,
-                    descriptionText: "Confirm your work schedule to automatically reduce the heating in your absence.",
+                    descriptionText:
+                        "Confirm your work schedule to automatically reduce the heating in your absence.",
                     titleText: "Heating an empty house?",
                   ),
                   CardTip(
                     topIcon: Icons.car_crash,
-                    descriptionText: "Some of your car trips would have been quicker by bus!",
+                    descriptionText:
+                        "Some of your car trips would have been quicker by bus!",
                     titleText: "38km of car to do by bus",
                   ),
                   CardTip(
                     topIcon: Icons.attach_money,
-                    descriptionText: "In the last month, these are your car trips of 2km or less, which you could have made on foot!",
+                    descriptionText:
+                        "In the last month, these are your car trips of 2km or less, which you could have made on foot!",
                     titleText: "Missed \$11.50 savings!",
                   )
                 ],
@@ -105,12 +166,24 @@ class Home extends HookWidget {
                 ),
               ),
             ),
-            const Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                CardSummary(topIcon: Icons.device_thermostat, titleText: '27°C', descriptionText: "Sunny", categoryText: "Temp."),
-                CardSummary(topIcon: Icons.electric_bolt, titleText: '370', descriptionText: "KW", categoryText: "Consumption"),
-                CardSummary(topIcon: Icons.air, titleText: '1-10', descriptionText: "Good", categoryText: "Air Quality"),
+                CardSummary(
+                    topIcon: Icons.device_thermostat,
+                    titleText: '${temperature.value}°C',
+                    descriptionText: weatherCondition.value,
+                    categoryText: "Temp."),
+                CardSummary(
+                    topIcon: Icons.electric_bolt,
+                    titleText: '370',
+                    descriptionText: "KW",
+                    categoryText: "Consumption"),
+                CardSummary(
+                    topIcon: Icons.air,
+                    titleText: getAqiRange(airPollution.value),
+                    descriptionText: getAqiText(airPollution.value),
+                    categoryText: "Air Quality"),
               ],
             )
           ],
