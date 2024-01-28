8e137211f7fe9bd567bef1230a9c017741cc574d
Thomas Mondor <thomas.mondor.1@ulaval.ca>
2024-01-28 10:26:08 -0500

    skeleton

--- lib/src/view/leftover/leftover.dart
+++ lib/src/view/leftover/leftover.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'package:skeletonizer/skeletonizer.dart';
 
 import 'package:amc_2024/src/theme/colors.dart';
 import 'package:amc_2024/src/view/leftover/leftover_item.dart';
@@ -10,12 +11,11 @@ import '../../application/location_service.dart';
 import '../../domain/place/place_model.dart';
 import '../../infra/places/places_api.dart';
 
-class Leftover extends HookWidget{
+class Leftover extends HookWidget {
   const Leftover({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     final stores = useState(<PlaceModel>[]);
 
     useEffect(() {
@@ -25,7 +25,8 @@ class Leftover extends HookWidget{
 
         if (await locationService.requestPermission()) {
           final location = await locationService.getCurrentLocation();
-          List<PlaceModel> places = await placesApi.getNearbyStores(location.latitude, location.longitude);
+          List<PlaceModel> places = await placesApi.getNearbyStores(
+              location.latitude, location.longitude);
           stores.value = places.sublist(0, min(places.length, 10));
         }
       }
@@ -36,52 +37,51 @@ class Leftover extends HookWidget{
 
     if (stores.value.isNotEmpty) {
       return Scaffold(
-            appBar: AppBar(
-              automaticallyImplyLeading: false,
-              backgroundColor: Colors.white,
-              surfaceTintColor: Colors.white,
-              title: Align(
-                alignment: Alignment.center,
-                child: Text(
-                  'Leftover',
-                  style: Theme.of(context)
-                      .textTheme
-                      .displayLarge!
-                      .copyWith(color: kcPrimaryVariant),
-                ),
-              ),
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.white,
+          surfaceTintColor: Colors.white,
+          title: Align(
+            alignment: Alignment.center,
+            child: Text(
+              'Leftover',
+              style: Theme.of(context)
+                  .textTheme
+                  .displayLarge!
+                  .copyWith(color: kcPrimaryVariant),
             ),
-            body: SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                // child: Expanded(
-                child: ListView.builder(
-                  itemCount: stores.value.length,
-                  itemBuilder: (context, index) {
-                    if (index == 0) {
-                      return Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Text(
+          ),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            // child: Expanded(
+            child: ListView.builder(
+                itemCount: stores.value.length,
+                itemBuilder: (context, index) {
+                  if (index == 0) {
+                    return Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
                           'Save on near-expiration foods and help businesses reduce their food waste!',
                           style: Theme.of(context)
                               .textTheme
                               .bodyLarge!
                               .copyWith(color: kcPrimaryVariant)),
-                      );
-                    }
-                    return Padding(
-                      padding: const EdgeInsets.only(bottom: 4.0),
-                      child: LeftoverItem(
-                        storeName: stores.value[index].name,
-                        distanceToWalk: stores.value[index].distance,
-                        ecoScore: Random().nextInt(5) + 1,
-                      ),
                     );
                   }
-                ),
-              ),
-            ),
-          );
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 4.0),
+                    child: LeftoverItem(
+                      storeName: stores.value[index].name,
+                      distanceToWalk: stores.value[index].distance,
+                      ecoScore: Random().nextInt(5) + 1,
+                    ),
+                  );
+                }),
+          ),
+        ),
+      );
     } else {
       return Scaffold(
         appBar: AppBar(
@@ -99,16 +99,38 @@ class Leftover extends HookWidget{
             ),
           ),
         ),
-        body: const SafeArea(
+        body: SafeArea(
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24),
-            child: Center(
-              child: CircularProgressIndicator(),
-            ),
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            // child: Expanded(
+            child: ListView.builder(
+                itemCount: 6,
+                itemBuilder: (context, index) {
+                  if (index == 0) {
+                    return Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                          'Save on near-expiration foods and help businesses reduce their food waste!',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyLarge!
+                              .copyWith(color: kcPrimaryVariant)),
+                    );
+                  }
+                  return const Skeletonizer(
+                    enabled: true,
+                    child: Padding(
+                      padding: EdgeInsets.only(bottom: 4.0),
+                      child: ListTile(
+                        title: Bone.text(words: 2,),
+                        subtitle: Bone.multiText(lines: 2),
+                      ),
+                    ),
+                  );
+                }),
           ),
         ),
       );
-
     }
   }
 }
